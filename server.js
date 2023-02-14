e12462519d60c7124563ce32ec2c4e97fe47d4a8
Hanan Ahmed <hanan@Hanans-MacBook-Pro.local>
2023-02-14 21:50:07 +0000

    Added Swagger for API Docs

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 function Server() {
 	const app = express();
+
+	app.use(
+		'/api-docs',
+		swaggerUi.serve,
+		swaggerUi.setup(swaggerDocument)
+	);
 
 	const PORT = process.env.PORT;
 
